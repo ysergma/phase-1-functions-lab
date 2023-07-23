ea1c18aa7b5e540a487c0d7b1069e1aeb32a073c
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-07-23 21:48:10 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+ function distanceFromHqInBlocks(distance) {
+    //returns the number of blocks given a value
+    distance = Number.parseInt(distance);
+    return (Math.abs(distance - 42));
+  }
+
+  function distanceFromHqInFeet(distance) {
+  distanceFromHqInBlocks(distance);
+      return (Math.abs((distance*264) - (42*264)));
+
+}
+  function distanceTravelledInFeet (start,destination) {
+    //returns the number of feet traveled
+      var distance=Math.abs((destination - start) *264);
+      console.log("distance+1");
+      return distance;
+  }
+
+function calculatesFarePrice(start, destination) {
+    //returns the number of feet traveled
+    var distance=Math.abs((destination*264) - (start*264));
+    if (distance <= 400) {
+        return  0
+    } else if (distance > 400 && distance <= 2000 ) {
+        let price = ((distance-400) * 2)/100
+        return price
+    }else if (distance > 2000 && distance <= 2500 ){
+        let price = 25
+        return price
+    }else if (distance > 2500 ){
+        return "cannot travel that far";
+
+  }
+   }
